1153c7c44a683c636f11323cc00ab2d8a1f4e53b
rebeccabosse <rebeccabosse@hotmail.fr>
2024-07-31 20:33:18 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import TypeBar from "./components/Types/TypeBar";
+
 import axios from "axios";
 
 function App() {
-  const [types, setTypes] = useState([]);
-
-  useEffect(() => {
-    const fetchTypes = async () => {
-      const response = await axios.get("https://pokeapi.co/api/v2/type");
-      const filterType = response.data.results.filter(
-        (res) => res.name !== "unknown"
-      );
-      setTypes(filterType);
-      console.log(filterType);
-    };
-    fetchTypes();
-  }, []);
-
   return (
     <div className="App">
       <Navbar />
-      <TypeBar types={types} />
-      <Home />
     </div>
   );
 }
