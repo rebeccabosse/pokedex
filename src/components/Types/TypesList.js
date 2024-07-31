015dbc492a002cbf49be428d9e69f8f35525a55b
rebeccabosse <rebeccabosse@hotmail.fr>
2024-08-01 00:11:13 +0200

    filter type unknown

--- src/components/Types/TypesList.js
+++ src/components/Types/TypesList.js
@@ -5,10 +5,12 @@ import "./types.css";
 const TypesList = (props) => {
   const { typeList } = props;
   console.log(typeList);
+  const filterType = typeList.filter((type) => type.name !== "unknown");
+  console.log(filterType);
   return (
     <section className="type-list">
-      {typeList &&
-        typeList.map((t, i) => {
+      {filterType &&
+        filterType.map((t, i) => {
           return <ButtonType name={t.name} key={i} type={t.name} />;
         })}
     </section>
