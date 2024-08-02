511c3b8a0a78a74d1fb3db9df2d85256cae05fb9
rebeccabosse <rebeccabosse@hotmail.fr>
2024-08-02 15:34:43 +0200

    add home to app.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import NavTypes from "./components/Types/NavTypes";
+import NavBar from "./components/Navbar";
+import Home from "./components/Home";
 
-const buttons = [];
 function App() {
-  const [isSelected, setIsSelected] = useState(0);
-  const [typeList, setTypeList] = useState([]);
-
-  useEffect(() => {
-    const getTypes = async () => {
-      const response = await axios.get("https://pokeapi.co/api/v2/type/");
-      response.data.results.forEach(async (id) => {
-        const pokeType = await axios.get(
-          `https://pokeapi.co/api/v2/type/${id.name}`
-        );
-        setTypeList((t) => [...t, pokeType.data]);
-      });
-    };
-    getTypes();
-  }, []);
-
-  //console.log(...typeList);
-  const buttons = [typeList];
-
   return (
     <div className="App">
-      <Navbar />
-      <NavTypes typeList={typeList} />
+      <NavBar />
+      <Home />
     </div>
   );
 }
