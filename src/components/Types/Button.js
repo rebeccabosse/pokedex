e00cb501b94ec139a0cb103fdc2effc98b2ded21
rebeccabosse <rebeccabosse@hotmail.fr>
2024-08-03 17:51:16 +0200

    Buttons types

---
+++ src/components/Types/Button.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+import "./types.css";
+import { Link } from "react-router-dom";
+
+const Button = ({ name, type, id, pokemon }) => {
+  /*const handleCLick = () => {
+    pokemon &&
+      pokemon.forEach((p) => {
+        let po = p.pokemon.url;
+
+        console.log(po);
+      });
+  };
+  const listPokemon =
+    pokemon &&
+    pokemon.name?.map((pok) => {
+      <div>{pok.name}</div>;
+    });
+  console.log(listPokemon);
+  */
+  return (
+    <>
+      <Link key={id} className={`btn-type ${type}`} to={`${type}`}>
+        {name}
+      </Link>
+    </>
+  );
+};
+
+export default Button;
