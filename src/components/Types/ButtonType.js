10455e465a0c0f47958f9ebc26574804e3fd58e1
rebeccabosse <rebeccabosse@hotmail.fr>
2024-08-03 16:22:53 +0200

    buttons

--- src/components/Types/ButtonType.js
+++ src/components/Types/ButtonType.js
@@ -1,19 +1,31 @@
 import React from "react";
-
 import "./types.css";
+import Button from "./Button";
+import PokemonList from "../Pokemons/PokemonList";
 
 function ButtonType({ typeList }) {
   const filterType = typeList.filter(
     (type) => type.name !== "unknown" && type.pokemon.length > 0
   );
 
-  console.log(filterType);
   return (
-    <div id="btn-nav">
-      {filterType.map((type) => {
-        return <button key={type.id}>{type.name}</button>;
-      })}
-    </div>
+    <>
+      <h1 className="intro">choose a type</h1>
+      <div className="type-list ">
+        {filterType.map((type, id) => {
+          return (
+            <Button
+              key={id}
+              name={type.name}
+              type={type.name}
+              id={type.id}
+              pokemon={type.pokemon}
+              buttons={type.pokemon.url}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 }
 
