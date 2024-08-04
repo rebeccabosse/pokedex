81c25cb8f66d936a45edcc7e34b5b78f1a6a4801
rebeccabosse <rebeccabosse@hotmail.fr>
2024-08-04 16:08:17 +0200

    fetch info pokemon

--- src/components/Pokemons/PokemonRandom.js
+++ src/components/Pokemons/PokemonRandom.js
@@ -1,34 +1,43 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Card from "./Card";
+import CardRandom from "./CardRandom";
 
 const PokemonRandom = () => {
   const [pokemons, setPokemons] = useState([]);
+
   useEffect(() => {
     const getPokemons = async () => {
       const response = await axios.get(
-        "https://pokeapi.co/api/v2/pokemon?limit=30&offset=0"
+        "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
       );
-      response.data.results.forEach(async (pokemon) => {
-        const poke = await axios.get(
-          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
-        );
-        setPokemons(poke.data);
-        /*
-        setPokemons((state) => {
-          state = [...state, poke.data];
-          state.sort((a, b) => (a.id > b.id ? 1 : -1));
-          return state;
-        });
-        */
-      });
+      const pokemonUrl = response.data.results.map((pokemon) => pokemon.url);
+
+      const pokeData = await Promise.all(
+        pokemonUrl.map(async (url) => {
+          const pokemonResponse = await axios.get(url);
+          const speciesResponse = await axios.get(
+            pokemonResponse.data.species.url
+          );
+          const evolutionResponse = await axios.get(
+            speciesResponse.data.evolution_chain.url
+          );
+          return {
+            ...pokemonResponse.data,
+            species: speciesResponse.data,
+            evolution_chain: evolutionResponse.data,
+          };
+        })
+      );
+      setPokemons(pokeData);
+
+      console.log(pokeData);
     };
+
     getPokemons();
   }, []);
-
   return (
     <div style={{ backgroundColor: "lightseagreen", marginTop: "30px" }}>
-      <Card pokemons={pokemons} />
+      <CardRandom pokemons={pokemons} />
     </div>
   );
 };
