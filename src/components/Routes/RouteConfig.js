a733aee49512333de4ec52592a5a13fe27ba94d1
rebeccabosse <rebeccabosse@hotmail.fr>
2024-08-24 14:56:01 +0200

    add pageHome

--- src/components/Routes/RouteConfig.js
+++ src/components/Routes/RouteConfig.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-
 import Home from "../Home";
 import PokemonList from "../Pokemons/PokemonList";
+import PageHome from "../PagePokemon/PageHome";
 function RouteConfig() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path=":name" element={<PokemonList />} />
+        <Route path=":type" element={<PokemonList />} />
+        <Route path=":type/:name" element={<PageHome />} />
       </Routes>
     </>
   );
