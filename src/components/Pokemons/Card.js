bb19e97de2b1071d2a2a8b1ad5f863135f7589c7
rebeccabosse <rebeccabosse@hotmail.fr>
2024-08-24 13:23:03 +0200

    card

--- src/components/Pokemons/Card.js
+++ src/components/Pokemons/Card.js
@@ -9,23 +9,24 @@ const Card = ({ pokemons }) => {
         className={`card-pokemon`}
         style={{ backgroundColor: COLOR.TYPE(pokemons.types[0].type.name) }}
       >
-        <p className="poke-id">#{pokemons.id}</p>
-        <div className="poke-header">
-          <div className="poke-info">
-            <h3 className="poke-name">{pokemons.name}</h3>
-            <div className="types-pokemon">
-              {pokemons.types.map(({ type }, i) => (
+        <div className="card-header">
+          <div className="poke-header">
+            <h3 className="poke-name"> {pokemons.name}</h3>
+            <h4 className="poke-id"> #{pokemons.id}</h4>
+          </div>
+          <div className="poke-infos">
+            <div className="poke-types">
+              {pokemons.types?.map(({ type }, i) => (
                 <li key={i}>{type.name}</li>
               ))}
             </div>
-          </div>
-
-          <div className="image-content">
-            <img
-              className="img-pokemon"
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemons.id}.png`}
-              alt="pokemon"
-            />
+            <div className="image-content">
+              <img
+                className="img-pokemon"
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemons.id}.png`}
+                alt="pokemon"
+              />
+            </div>
           </div>
         </div>
       </div>
