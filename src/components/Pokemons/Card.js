2433886e76bc935433364fc9d5b9d6fed5f31554
rebeccabosse <rebeccabosse@hotmail.fr>
2024-08-24 14:56:41 +0200

    clean info

--- src/components/Pokemons/Card.js
+++ src/components/Pokemons/Card.js
@@ -1,35 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./pokemonCard.css";
 import { COLOR } from "../Types/TypeColors";
 const Card = ({ pokemons }) => {
-  console.log(pokemons);
+  //console.log(pokemons);
   return (
     <>
-      <div
-        className={`card-pokemon`}
-        style={{ backgroundColor: COLOR.TYPE(pokemons.types[0].type.name) }}
-      >
-        <div className="card-header">
-          <div className="poke-header">
-            <h3 className="poke-name"> {pokemons.name}</h3>
-            <h4 className="poke-id"> #{pokemons.id}</h4>
-          </div>
-          <div className="poke-infos">
-            <div className="poke-types">
-              {pokemons.types?.map(({ type }, i) => (
-                <li key={i}>{type.name}</li>
-              ))}
+      <Link to={`${pokemons.name}`}>
+        <div
+          className={`card-pokemon`}
+          style={{ backgroundColor: COLOR.TYPE(pokemons.types[0].type.name) }}
+        >
+          <div className="card-header">
+            <div className="poke-header">
+              <h3 className="poke-name"> {pokemons.name}</h3>
+              <h4 className="poke-id"> #{pokemons.id}</h4>
             </div>
-            <div className="image-content">
-              <img
-                className="img-pokemon"
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemons.id}.png`}
-                alt="pokemon"
-              />
+            <div className="poke-infos">
+              <div className="poke-types">
+                {pokemons.types?.map(({ type }, i) => (
+                  <li key={i}>{type.name}</li>
+                ))}
+              </div>
+              <div className="image-content">
+                <img
+                  className="img-pokemon"
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemons.id}.png`}
+                  alt="pokemon"
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </Link>
     </>
   );
 };
