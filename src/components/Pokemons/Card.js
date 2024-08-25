a847128c2faddb177fd32fa10b89244bd6a46bb3
rebeccabosse <rebeccabosse@hotmail.fr>
2024-08-25 17:45:03 +0200

    upgrade css

--- src/components/Pokemons/Card.js
+++ src/components/Pokemons/Card.js
@@ -6,33 +6,34 @@ const Card = ({ pokemons }) => {
   //console.log(pokemons);
   return (
     <>
-      <Link to={`${pokemons.name}`}>
-        <div
-          className={`card-pokemon`}
-          style={{ backgroundColor: COLOR.TYPE(pokemons.types[0].type.name) }}
-        >
-          <div className="card-header">
-            <div className="poke-header">
+      <div
+        className={`card-pokemon`}
+        style={{ backgroundColor: COLOR.TYPE(pokemons.types[0].type.name) }}
+      >
+        <div className="card-header">
+          <div className="poke-header">
+            <Link to={`${pokemons.name}`} style={{ textDecoration: "none" }}>
               <h3 className="poke-name"> {pokemons.name}</h3>
-              <h4 className="poke-id"> #{pokemons.id}</h4>
+            </Link>
+
+            <h4 className="poke-id"> #{pokemons.id}</h4>
+          </div>
+          <div className="poke-infos">
+            <div className="poke-types">
+              {pokemons.types?.map(({ type }, i) => (
+                <li key={i}>{type.name}</li>
+              ))}
             </div>
-            <div className="poke-infos">
-              <div className="poke-types">
-                {pokemons.types?.map(({ type }, i) => (
-                  <li key={i}>{type.name}</li>
-                ))}
-              </div>
-              <div className="image-content">
-                <img
-                  className="img-pokemon"
-                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemons.id}.png`}
-                  alt="pokemon"
-                />
-              </div>
+            <div className="image-content">
+              <img
+                className="img-pokemon"
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemons.id}.png`}
+                alt="pokemon"
+              />
             </div>
           </div>
         </div>
-      </Link>
+      </div>
     </>
   );
 };
